7b4da3422b31b86a071efee5b12e6276ea3dbc8c
LordMath <92640773+LG-LordMath@users.noreply.github.com>
2023-04-03 23:26:34 +0200

    update

---
+++ myfirstgame/lib/engine/bullets/NormalBullet.dart
@@ -0,0 +1,95 @@
+
+
+import 'dart:ui';
+
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flame_audio/flame_audio.dart';
+import 'package:flutter/material.dart';
+import 'package:myfirstgame/engine/ships/BasicShip.dart';
+import 'package:myfirstgame/game/MySpaceGame.dart';
+
+import '../basics/MovementDirection.dart';
+
+class NormalBullet extends SpriteComponent with CollisionCallbacks, HasGameRef<MySpaceGame>
+{
+  final _defaultColor = Colors.red;
+  Vector2 _enemy = Vector2(0, 0);
+  Vector2 _currentship = Vector2(0, 0);
+  late int _team;
+  final String _imagepath = 'laser.png';
+  final double _imagesizex = 10;
+  final double _imagesizey = 30;
+  final int movementspeed = 2;
+  int lifetime = 300;
+  final int damage = 100;
+  bool _isplayingmusic = false;
+
+  NormalBullet(this._currentship, this._enemy, this._team){
+  }
+  @override
+  Future<void> onLoad() async
+  {
+    super.onLoad();
+    if(_imagepath.isNotEmpty){
+      sprite = await gameRef.loadSprite(_imagepath);
+    }
+      size = Vector2(_imagesizex, _imagesizey);
+      position = Vector2(50, 0);
+    final defaultPaint = Paint()
+      ..color = _defaultColor
+      ..style = PaintingStyle.stroke ;
+    ShapeHitbox hitbox = CircleHitbox()
+      ..paint = defaultPaint
+      ..renderShape = true;
+      add(hitbox);
+    if(!_isplayingmusic)
+    {
+      FlameAudio.play('Laser.ogg')..timeout(Duration(seconds: 2));
+      _isplayingmusic = true;
+
+    }
+
+
+
+
+
+    final effect = MoveEffect.to(_enemy - _currentship, EffectController(duration: 5));
+    add(effect);
+  }
+
+  @override
+  void update(double dt){
+    super.update(dt);
+
+
+    lifetime--;
+    if(lifetime <=  0){
+      removeFromParent();
+    }
+
+
+  }
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints,
+      PositionComponent other,
+      ) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is ScreenHitbox) {
+      removeFromParent();
+    }else if(other is BasicShip)
+    {
+      if(other.currentteam != _team){
+        removeFromParent();
+      }
+    }
+  }
+
+  int get team => _team;
+
+  set team(int value) {
+    _team = value;
+  }
+}
---
+++ myfirstgame/lib/engine/ships/seperatistencis/SeperatistCISShipLoader.dart
@@ -0,0 +1,56 @@
+
+
+
+class RepublicShipsLoader{
+
+
+  static Map<EnumRepublicShips, BasicShip> empireships  = {};
+
+  void load()  {
+    republicships[EnumRepublicShips.Acclamator] = Acclamator(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.ARC170] = ARC170(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.Arquitens] = Arquitens(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.CR90Corvette] = CR90Corvette(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.ETA2Actis] = ETA2Actis(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.NuAttackShuttle] = NuAttackShuttle(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.Venator] = Venator(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.YWing] = YWing(0, 0, 40, 50, 1);
+    republicships[EnumRepublicShips.Z95] = Z95(0, 0, 40, 50, 1);
+
+
+    //   republicships[EnumNation.Imperium] = BasicNation("Galactic Empire", ImageLoader.sprites[EnumImages.IconImperium]!, "The Galactic Empire, also known as the First Galactic Empire, the Empire and later as the Old Empire, was the monarchy that replaced the Galactic Republic at the end of the Clone Wars. Though the Empire rose to power by promising to bring order to the galaxy, it oversaw a period of harsh military rule and aggressive expansion.");
+
+  }
+
+  void reloadObject(EnumRepublicShips ship)
+  {
+    switch (ship){
+      case EnumRepublicShips.Acclamator:
+        republicships[EnumRepublicShips.Acclamator] = Acclamator(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.ARC170:
+        republicships[EnumRepublicShips.ARC170] = ARC170(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.Arquitens:
+        republicships[EnumRepublicShips.Arquitens] = Arquitens(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.CR90Corvette:
+        republicships[EnumRepublicShips.CR90Corvette] = CR90Corvette(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.ETA2Actis:
+        republicships[EnumRepublicShips.ETA2Actis] = ETA2Actis(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.NuAttackShuttle:
+        republicships[EnumRepublicShips.NuAttackShuttle] = NuAttackShuttle(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.Venator:
+        republicships[EnumRepublicShips.Venator] = Venator(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.YWing:
+        republicships[EnumRepublicShips.YWing] = YWing(0, 0, 40, 50, 1);
+        break;
+      case EnumRepublicShips.Z95:
+        republicships[EnumRepublicShips.Z95] = Z95(0, 0, 40, 50, 1);
+        break;
+    }
+  }
